9ebac799f7bf8e62cb973a67f8e74bb737f369f8
Philipp Melab <philipp.melab@gmail.com>
2017-10-18 18:03:49 +0200

    Migrated graphql_link to graphql_core.

---
+++ graphql_link/src/Plugin/GraphQL/Types/LinkItem.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Drupal\graphql_link\Plugin\GraphQL\Types;
+
+use Drupal\graphql\Plugin\GraphQL\Types\TypePluginBase;
+
+/**
+ * GraphQL type for link field results.
+ *
+ * @GraphQLType(
+ *   id = "link_item",
+ *   name = "LinkItem"
+ * )
+ */
+class LinkItem extends TypePluginBase {
+
+}
